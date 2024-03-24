32d357c8f8fa235fab3b6d7ec281cdb03d3b55f4
vaheed9440 <124774319+vaheed9440@users.noreply.github.com>
2024-03-24 15:25:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -361,7 +361,7 @@ output "lb_dns_name" {
 
 
 resource "aws_s3_bucket" "my_bucket" {
-  bucket = "abdulvaheed789654"  
+  bucket = "abdusublvaheed788479654"  
 
   acl    = "private"  
   versioning {
